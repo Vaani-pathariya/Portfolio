9451c5de97f615d7e2197be18eee8a282131e144
Vaani <vaanipathariya@gmail.com>
2024-08-11 12:01:55 +0530

    (fix) : Width in navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,43 +11,43 @@ const Navbar = () => {
         <div className="pt-3 fixed w-full">
         <div className="bg-lightBlack w-10/12 rounded-3xl mx-auto flex text-lg border border-gray-400">
             <div
-                className={`px-3 rounded-3xl my-1.5 mx-2 py-1.5 cursor-pointer ${page === "Home" ? "bg-newGreen" : ""}`}
+                className={`px-6 rounded-3xl my-1.5 mx-2 py-1.5 cursor-pointer ${page === "Home" ? "bg-newGreen" : ""}`}
                 onClick={() => changeNavbar("Home")}
             >
                 Home
             </div>
             <div
-                className={`px-3 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Skills" ? "bg-newGreen" : ""}`}
+                className={`px-6 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Skills" ? "bg-newGreen" : ""}`}
                 onClick={() => changeNavbar("Skills")}
             >
                 Skills
             </div>
             <div
-                className={`px-3 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Experience" ? "bg-newGreen" : ""}`}
+                className={`px-6 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Experience" ? "bg-newGreen" : ""}`}
                 onClick={() => changeNavbar("Experience")}
             >
                 Experience
             </div>
             <div
-                className={`px-3 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Resume" ? "bg-newGreen" : ""}`}
+                className={`px-6 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Resume" ? "bg-newGreen" : ""}`}
                 onClick={() => changeNavbar("Resume")}
             >
                 Resume
             </div>
             <div
-                className={`px-3 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Achievements" ? "bg-newGreen" : ""}`}
+                className={`px-6 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Achievements" ? "bg-newGreen" : ""}`}
                 onClick={() => changeNavbar("Achievements")}
             >
                 Achievements
             </div>
             <div
-                className={`px-3 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Projects" ? "bg-newGreen" : ""}`}
+                className={`px-6 rounded-3xl my-1.5 mx-auto py-1.5 cursor-pointer ${page === "Projects" ? "bg-newGreen" : ""}`}
                 onClick={() => changeNavbar("Projects")}
             >
                 Projects
             </div>
             <div
-                className={`px-3 rounded-3xl my-1.5 mx-2 py-1.5 cursor-pointer ${page === "Contact" ? "bg-newGreen" : ""}`}
+                className={`px-6 rounded-3xl my-1.5 mx-2 py-1.5 cursor-pointer ${page === "Contact" ? "bg-newGreen" : ""}`}
                 onClick={() => changeNavbar("Contact")}
             >
                 Contact
